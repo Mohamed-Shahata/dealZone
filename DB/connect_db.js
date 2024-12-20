9909c1b37eeebf3d56366561ab4507b38a0a5c4d
mohamed <mohamedmrslan@gmail.com>
2024-12-20 18:48:51 +0200

    update mongodb

--- DB/connect_db.js
+++ DB/connect_db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connection_db = async () => {
   return await mongoose
-    .connect(process.env.MONGODB_URL)
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+    })
     .then(() => console.log("connected mongodb ✔️"))
     .catch((err) => console.log("mongodb err ✖️: ", err))
 };
